67986362e5d07f1d861ecdf519d6cb147c06816b
jimo1001 <jimo1001@gmail.com>
2010-12-10 00:58:39 +0900

    add keybinds.js

---
+++ keybind/keybinds.js
@@ -0,0 +1,188 @@
+/**
+ * keybinds.js
+ * @author jimo1001
+ */
+
+
+/**
+ * Factory of Keybind
+ */
+var keybindFactory = {
+    _event_binding_elements: null,
+    _event_type: 'keydown',
+    _keybinds: [],
+    _keys: {
+        9: "TAB",
+        27: "ESC",
+        33: "PageUp",
+        34: "PageDown",
+        35: "End",
+        36: "Home",
+        37: "Left",
+        38: "Up",
+        39: "Right",
+        40: "Down",
+        45: "Insert",
+        46: "Delete",
+        112: "F1",
+        113: "F2",
+        114: "F3",
+        115: "F4",
+        116: "F5",
+        117: "F6",
+        118: "F7",
+        119: "F8",
+        120: "F9",
+        121: "F10",
+        122: "F11",
+        123: "F12"
+    },
+    _skeys: {
+        8: "BS",
+        10: "RET",
+        13: "RET",
+        32: "SPC"
+    },
+    _mkeys: {
+        'altKey': "A",
+        'ctrlKey': "C",
+        'metaKey': "M",
+        'shiftKey': "S"
+    },
+
+    _create: function(element, key, callback, force) {
+        var self = this;
+        return new (function Keybind(element, key, callback, force) {
+            this.element = element;
+            this.key = key;
+            this.callback = callback;
+            this.force = (force === undefined) ? false : force;
+
+            this.execute = function(evt) {
+                if (!this.force)
+                    if (self.isForm(evt.target)) return;
+                this.callback.call(this.element, evt, this);
+            }
+        })(element, key, callback, force);
+    },
+
+    init: function() {
+        if (this._event_binding_elements && (this._event_binding_elements.length > 0))
+            this.unbind();
+        this.bind();
+    },
+
+    bind: function(elem) {
+        if (!elem) elem = window;
+        var ebe = this._event_binding_elements;
+        if (ebe === null) { ebe = []; }
+        if (elem in ebe) return;
+        elem.addEventListener(this._event_type, this._listener, false);
+        ebe.push(elem);
+    },
+
+    unbind: function(elem) {
+        var ebe = this._event_binding_elements;
+        if (!ebe || (ebe.length === 0)) return;
+        var i;
+        for (i=0; i<ebe.length; i++) {
+            if ((elem === ebe[i]) || !elem) {
+                ebe[i].removeEventListener(this._event_type, this._listener, false);
+                delete ebe[i];
+            }
+        }
+    },
+
+    _listener: function(evt) {
+        var self = keybindFactory;
+        var key = self.getKeyname(evt);
+        if (key === "") return;
+        var kbs = self._keybinds, i = 0;
+        for (i=0; i<kbs.length; i++) {
+            console.log((kbs[i].key === key) && ((kbs[i].element === evt.target)), kbs[i].element, evt.target);
+            if ((kbs[i].key === key) && ((kbs[i].element === evt.target) || (/^(?:\[object DOMWindow\]|\[object HTMLDocument\])$/.test(kbs[i].element.toString())))) {
+                kbs[i].execute(evt);
+            }
+        }
+    },
+
+    /**
+     * create Keybind object.
+     * @param element a DOM element
+     * @param key shortcut key (e.x. Control + y -> C-y)
+     * @param callback a callback function
+     * @param force execute the callback even where at the input/textarea
+     */
+    add: function(element, key, callback, force) {
+        if (!element || !key) { return null; }
+        if (this._event_binding_elements === null) {
+            this.init();
+        }
+        var kb = this._create(element, key, callback, force);
+        this._keybinds.push(kb);
+        return kb;
+    },
+
+    getKeybinds: function() {
+        return this._keybinds;
+    },
+
+    getKeyname: function(evt, isModiferKey) {
+        var key = [], k = '';
+        var mkeys = this._mkeys;
+        for (var mk in mkeys) {
+            if (evt[mk] && mkeys.hasOwnProperty(mk)) {
+                if (isModiferKey) return mk;
+                if ((mk == "metaKey") && (evt["ctrlKey"] === true))
+                    continue;
+                key.push(this._mkeys[mk]);
+            }
+        }
+        if (isModiferKey) return "";
+        if (evt.which) {
+            k = this._skeys[evt.which] || this._keys[evt.which] || String.fromCharCode(evt.which).toLowerCase();
+        } else if (evt.keyCode) {
+            k = this._keys[evt.keyCode];
+        }
+
+        if (/^(?:[a-zA-Z0-9]+)$/.test(k)) {
+            key.push(key.length ? '-'+k : k);
+            return key.join('');
+        } else {
+            return "";
+        }
+
+    },
+
+    remove: function(keybind) {
+        if (keybind) {
+            this._keybinds = this._keybinds.filter(function(bind) {
+                return (keybind != bind);
+            });
+        }
+    },
+
+    removeByKeyname: function(element, key) {
+        this._keybinds = this._keybinds.filter(function(keybind) {
+            return (!(keybind.element == element && keybind.key == key));
+        });
+    },
+
+    removeAll: function() {
+        var kbs = this._keybinds, i = 0;
+        if (!kbs || kbs.length === 0) {
+            return;
+        }
+        for (i=0; i<kbs.length; i++) {
+            delete kbs[i];
+        }
+    },
+
+    /**
+     * the method checks whether a input field or a textarea.
+     * @return boolean if the node is input or textarea, return true.
+     */
+    isForm: function(node) {
+        return /^(?:input|textarea)$/.test(node.nodeName.toLowerCase());
+    }
+};
